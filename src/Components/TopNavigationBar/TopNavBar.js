c38f0cc58dcce0a6fad05675f8095b1aa020c518
Dan  Machlenkin <danmachlenkin10@gmail.com>
2022-09-13 19:29:50 +0300

    creating top navigation bar

---
+++ src/Components/TopNavigationBar/TopNavBar.js
@@ -0,0 +1,19 @@
+import React, { Fragment } from "react";
+
+//CSS imports
+import modules from "./TopNavBar.module.css";
+import image from '../images/logo.png';
+
+const TopNavBar = (props) => {
+  return (
+    <Fragment>
+      <div className={modules.container}>
+        <img className={modules.img} src={image} alt='logo' width="65" height="65" />
+        <h2>Business name</h2>
+        <h3 className={modules.owner}>Owners name</h3>
+      </div>
+    </Fragment>
+  );
+};
+
+export default TopNavBar;
