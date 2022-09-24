30116a40dd88d3294851d16090c8f6427b251778
Dan  Machlenkin <danmachlenkin10@gmail.com>
2022-09-24 22:26:08 +0300

    working on the time range selection logic in the Reports page

---
+++ src/Components/Reports/CheckBoxs.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+//JS imports
+
+//CSS imports
+
+
+const options = ["Today", "Yesterday", "Last 7 Days", "Last 30 Days"];
+
+const CheckBoxs = (props) => {
+  const [checkedState, setCheckedState] = useState(
+    new Array(options.length).fill(false)
+  );
+
+  const onChangeHandler = (position) => {
+    let updatedCheckedState = checkedState.map((item,index) => {
+       return position === index ? !item : false;
+    })
+
+    setCheckedState(updatedCheckedState);
+  }
+
+  
+  return (
+    <div>
+      {options.map((option, index) => {
+        return (
+          <div>
+            <input type="checkbox"  checked={checkedState[index]} onChange={ ()=> onChangeHandler(index)}/>
+            <label>{option}</label>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default CheckBoxs;
--- src/Components/Reports/TimeRange.js
+++ src/Components/Reports/TimeRange.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import Calendar from "react-calendar";
 
 //JS imports
-
+import CheckBoxs from "./CheckBoxs";
 //CSS imports
 import modules from './TimeRange.module.css';
 import 'react-calendar/dist/Calendar.css';
@@ -19,14 +19,15 @@ const TimeRange = () => {
     <div className={modules.contentWrapper}>
       <Calendar onChange={changeCurrentDate} value={currentDate} />
       <div className={modules.checkBoxesContainer}>
-        <input type="checkbox" id="today" name="today" />
+        <CheckBoxs/>
+        {/* <input type="checkbox" id="today" name="today" />
         <label htmlFor="today">Today</label><br/>
         <input type="checkbox" id="yesterday" name="yesterday" />
         <label htmlFor="yesterday">Yesterday</label><br/>
         <input type="checkbox" id="7days" name="7days" />
         <label htmlFor="7days">Last 7 Days</label><br/>
         <input type="checkbox" id="30days" name="30days" />
-        <label htmlFor="30days">Last 30 Days</label><br/>
+        <label htmlFor="30days">Last 30 Days</label><br/> */}
       </div>
     </div>
   );
